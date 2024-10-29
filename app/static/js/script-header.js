bb5b81215af95bf3fb8b379bfc2fab295bd546af
ValeriiMedvedchuk <valerii.medvedchuk@gmail.com>
2024-10-29 10:20:53 +0200

    Folder stucture reorganization

--- app/static/js/script-header.js
+++
@@ -1,6 +0,0 @@
-const svg = document.querySelector('#id_svg');
-        const info = document.querySelector('.user_info_window');
-
-        svg.addEventListener('click', function () {
-            info.classList.toggle('show-right');
-        });
--- app/static/js/script-scrollbar.js
+++
@@ -1,8 +0,0 @@
-
-      let progress = document.getElementById('progressbar');
-      let zeeshan = document.body.scrollHeight - window.innerHeight;
-      window.onscroll = function(){
-           let progressHeight = (window.pageYOffset / zeeshan) * 104 + "%";
-          progress.style.height = progressHeight;
-  
-      }
